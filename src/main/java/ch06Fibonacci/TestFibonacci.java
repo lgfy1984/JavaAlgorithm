b70eeb66e8416841e2904a66384fe7369c59bc9d
Administrator <lgfy1984@gmail.com>
2019-08-25 13:53:03 +0800

    add

---
+++ src/main/java/ch06Fibonacci/TestFibonacci.java
@@ -0,0 +1,7 @@
+package ch06Fibonacci;
+
+public class TestFibonacci {
+	public static void main(String[] args) {
+		System.out.println(Fibonacci.getNumber(5));
+	}
+}
---
+++ src/main/java/ch18Graph/Vertex.java
@@ -0,0 +1,13 @@
+package ch18Graph;
+/**
+ * ∂•µ„¿‡
+ * @author Administrator
+ *
+ */
+public class Vertex {
+	private char label;
+	
+	public Vertex(char label) {
+		this.label = label;
+	}
+}
